497fd9aea690e1476f14e9f9285d2e1971438509
AsamiforWeb <95006342+AsamiforWeb@users.noreply.github.com>
2022-01-31 22:28:26 +0900

    Update digitalclock.js

--- digitalclock.js
+++ digitalclock.js
@@ -1,1 +1,21 @@
+function start(){
+  window,setInterval(clock, 1000);
+}
 
+
+function clock(){
+  
+  let now = new Date();
+  let h = now.getHours();
+  let m = now.getMinutes();
+  let s = now.getSeconds();
+  
+  h = h<10? "0" +h:h;
+  m = m<10? "0" +m:m;
+  s = s<10? "0" +s:s;
+  
+  let result = document.getElementById("result");
+  
+  result.textContent = h + " : " + m + " : " + s;
+  
+}
