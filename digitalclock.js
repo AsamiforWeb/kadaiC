83e7aea9b51b83b19cdba6ca125a16f5c00c0339
AsamiforWeb <95006342+AsamiforWeb@users.noreply.github.com>
2022-01-31 22:54:26 +0900

    Update digitalclock.js

--- digitalclock.js
+++ digitalclock.js
@@ -1,5 +1,5 @@
 function start(){
-  window.setInterval(clock, 1000);
+  window.setInterval(clock, 1000);//1000ミリ秒ごとにclock関数を繰り返す
 }
 
 
@@ -10,6 +10,7 @@ function clock(){
   let m = now.getMinutes();
   let s = now.getSeconds();
   
+  //一桁の場合、数字の前に0をつける
   if(h<10){
     h = " 0" + h;
   }else{
@@ -28,8 +29,8 @@ function clock(){
     s = s;
   }
   
-  let result = document.getElementById("result");
   
-  result.textContent = h + " : " + m + " : " + s;
+  
+  result.textContent = h + " : " + m + " : " + s;//時刻を表示
   
 }
